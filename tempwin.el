70fb2311465d61b32114e3c567246d8b570bd378
Koichi Osanai <osanai3@gmail.com>
2014-04-29 00:31:37 +0900

    initial commit

---
+++ tempwin.el
@@ -0,0 +1,27 @@
+;;; tempwin.el --- make temporary window -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014 by Koichi Osanai
+
+;; Author: Koichi Osanai <osanai3@gmail.com>
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'tempwin)
+
+;;; tempwin.el ends here
